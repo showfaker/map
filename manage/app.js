906fc47e6b9bde299648aab2f62a9821ac2bc531
逊然旖旎 <48502872+showfaker@users.noreply.github.com>
2019-04-03 12:49:04 +0800

    Update app.js
    服务器配置文件

--- manage/app.js
+++ manage/app.js
@@ -19,28 +19,9 @@ app.use(cors({
     allowMethods: ['GET', 'POST', 'DELETE'],
     allowHeaders: ['Content-Type', 'Authorization', 'Accept'],
 }))
-//配置session 中间件
-// app.keys = ['some secret hurr'];
-// const CONFIG = {
-//     key: 'koa-sess',
-//     maxAge: 646000,
-//     overwrite: true,
-//     httpOnly: true,
-//     signed: true,
-//     rolling: true,  /*每次请求都会重新设置session*/
-//     renew: false
-// };
-// app.use(session(CONFIG, app));
 //配置中间件及静态资源库
 app.use(bodyParser());
-// app.use(static(__dirname + '/static'));
-//配置模块
-// render(app, {
-//     root: path.join(__dirname, 'views'),
-//     extname: '.html',
-//     debug: process.env.NODE_ENV !== 'production',
-//     dateFormat:dateFormat=function(value){return sd.format(value, 'YYYY-MM-DD HH:mm');} /*扩展模板里面的方法*/
-// });
+
 //启动路由
 app.use(Router.routes()).use(Router.allowedMethods());
 Router.use(index);
